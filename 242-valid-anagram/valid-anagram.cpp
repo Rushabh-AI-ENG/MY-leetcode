0bb1cf8f59536cad179c463a12c18fdbc5cbec33
Rishabh The Devloper <rishabhgohil490@gmail.com>
2026-08-09 15:35:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (26.18%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+ 
+        unordered_map<char, int> sMap, tMap;
+ 
+        for (char c : s) {
+            sMap[c]++;
+        }
+ 
+        for (char c : t) {
+            tMap[c]++;
+        }
+ 
+        return sMap == tMap;
+    }
+};
